cec0d807cb356c50f48e7610d5cbf5ceac8eac2c
Gastón Celis <gastoncelis09@gmail.com>
2021-05-27 17:08:42 -0300

    Creacion del NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import  "./css/estilosNavBar.css";
+import logo from "./multimedia/imagenes/logoprofeciabiker.png";
+
+const NavBar = () => {
+    return ( 
+        <header className="encabezado">
+            <img src={logo}></img>
+            <h1 className="titulo">
+                <span>Profecía</span>
+                <span>Biker</span>
+            </h1>
+            <nav className="menu">
+                <a href="#"><p>Cascos</p></a>
+                <a href="#"><p>Indumentaria</p></a>
+                <a href="#"><p>Repuestos</p></a>
+                <a href="#"><p>Accesorios</p></a>
+                <a href="#"><p>Nosotros</p></a>
+                <a href="#"><p>Contacto</p></a>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
