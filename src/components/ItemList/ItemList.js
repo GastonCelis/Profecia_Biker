8c92fa9574cc880bdf13f830a295e3f88e28039b
Gastón Celis <gastoncelis09@gmail.com>
2021-06-20 18:43:30 -0300

    Se creo el filtrado por categorias

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,54 +1,20 @@
-import React, {useState, useEffect} from "react";
-import ItemListjson from "../../assets/informacion/ItemList.json"
+import React from "react";
 import ItemCount from "../ItemCount/ItemCount"
 import Item from "../Item/Item"
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 import { Link } from "react-router-dom";
-import "./ItemList.css"
+import "./ItemList.css";
 
-const ItemList = () => {
-    const [producto, setProducto] = useState([]);
-    const [load, setLoad] = useState(false)
-    useEffect(()=>{
-        setLoad(true)
-        setTimeout(()=>{
-            setLoad(false)
-        }, 2001)
-        const item = new Promise((resolve, reject) => {
-            setTimeout(()=> {
-                resolve(ItemListjson)
-            }, 2000);
-        })
-        item.then(resultado =>{
-            setProducto(resultado)
-        })
-    }, []);
 
-    return (
-        <div>
-            {
-                load ?
-                    <div className="loader">
-                        <ClimbingBoxLoader color={"#FF001F"} size={80}/>
-                    </div>
-                :
-
-                <div className="item-box-padre">
-                    {producto.map(elemento =>{
-                        return(
-                            <div className="item-box">
-                                <Link exact to={`/item/${elemento.id}`} className="nombre-item"><h2>{elemento.nombre}</h2></Link>
-                                <Item props={elemento} key={elemento.id}/>
-                                <ItemCount initial={1} stock={elemento.stock}/>
-                            </div>
-                        )
-                    })}
-                </div>
-            }
+const ItemList = ({props}) => {
+    return ( 
+        <div className="contenedor-itemlist">
+            <div className="item-box">
+                <Link exact to={`/item/${props.id}`} className="nombre-item"><h2>{props.nombre}</h2></Link>
+                <Item props={props} key={props.id}/>
+                <ItemCount initial={1} stock={props.stock}/>
+            </div>
         </div>
-        
-        
-    )
+    );
 }
 
 export default ItemList;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import ItemListjson from "../../assets/informacion/ItemList.json"
 import ItemList from "../ItemList/ItemList"
-import "./ItemListContainer.css";
-
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import "./ItemListContainer.css"
+import { useParams } from "react-router-dom";
 
 const ItemListContainer = () => {
-    return ( 
-        <div className="contenedor-itemlist">
+    const {id} = useParams()
+    const [producto, setProducto] = useState([]);
+    const [load, setLoad] = useState(false)
+    useEffect(()=>{
+        setLoad(true)
+        setTimeout(()=>{
+            setLoad(false)
+        }, 2001)
+        const item = new Promise((resolve, reject) => {
+            setTimeout(()=> {
+                resolve(ItemListjson)
+            }, 2000);
+        })
+        item.then(resultado =>{
+            setProducto(resultado)
+        })
+    }, []);
+
+    return (
+        <div>
             <h1 className="titulo">Bienvenid@ Biker</h1>
-            <ItemList/>
+            {
+                load ?
+                    <div className="loader">
+                        <ClimbingBoxLoader color={"#FF001F"} size={80}/>
+                    </div>
+                :
+
+                <div className="item-box-padre">
+                    {producto.map(elemento =>{
+                        if (elemento.categoria === id){
+                            <ItemList props={elemento} key={elemento.id}/>
+                        }
+                        else{
+                            return(
+                                <div>
+                                    <ItemList props={elemento} key={elemento.id}/>
+                                </div>
+                            )
+                        }
+                    })}
+                </div>
+            }
         </div>
-    );
+        
+        
+            
+        
+        
+    )
 }
 
 export default ItemListContainer;
