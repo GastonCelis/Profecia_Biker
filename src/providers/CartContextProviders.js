a9257067d91bbf187adb51bd76625afaa2cfb048
Gastón Celis <gastoncelis09@gmail.com>
2021-07-19 13:07:07 -0300

    Se agregaron validaciones en el cart

--- src/providers/CartContextProviders.js
+++ src/providers/CartContextProviders.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import CartContext from "../context/CartContext";
+import Swal from "sweetalert2/src/sweetalert2.js"
 
 const CartContextProviders = ({ defaultValue = [], children }) => {
     const [ cart, setCart ] = useState(defaultValue)
@@ -24,12 +25,57 @@ const CartContextProviders = ({ defaultValue = [], children }) => {
     }
 
     function removeItem(itemId){
-        const removeItem = cart.filter(prod => prod.infoProductos.id !== itemId)
-        setCart(removeItem)
+        Swal.fire({
+                title: '¿Desa eliminar este item del carrito?',
+                icon: "question",
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                showDenyButton: true,
+                confirmButtonText: `SI`,
+                denyButtonText: `NO`,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        const removeItem = cart.filter(prod => prod.infoProductos.id !== itemId)
+                        setCart(removeItem)
+                        Swal.fire({
+                            title: 'Item Eliminado',
+                            icon: "success",
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            allowEnterKey: false,
+                            confirmButtonText: `Aceptar`
+                        })
+                    }
+            })
+        
     }
 
     function clear(){
-        setCart([])
+        if(cart.length !== 0){
+            Swal.fire({
+                title: '¿Desa vaciar el carrito?',
+                icon: "question",
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                showDenyButton: true,
+                confirmButtonText: `SI`,
+                denyButtonText: `NO`,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        setCart([])
+                        Swal.fire({
+                            title: 'Carrito Vaciado',
+                            icon: "success",
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            allowEnterKey: false,
+                            confirmButtonText: `Aceptar`
+                        })
+                    }
+            })
+        }
     }
 
     function addItem(items){
